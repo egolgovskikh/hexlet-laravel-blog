3bf517b50f76d6ba8b848dd752d8ee88388bf77d
yevtea <egolgovskikh@gmail.com>
2019-09-13 15:58:31 +0300

    lesson 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
     <h1>О блоге</h1>
     <p>Эксперименты с Ларавелем на Хекслете</p>
-</body>
-</html>
+@endsection
